634ca1b4741c882327ea11fef68fd9bcdaaad56f
Mohamed Zeidan <mohamedzeidan2012@gmail.com>
2025-08-07 21:32:17 +0200

    Update Good.py

--- Good.py
+++ Good.py
@@ -2,23 +2,23 @@ import mpmath
 
 class PiGood:
     def __init__(self):
-        # Set initial precision
+        # Set initial precision for mpmath
         self.dps = 100000  # Starting with 100,000 digits
-        mpmath.mp.dps = self.dps  # Set precision
-        self.pi = str(mpmath.pi)
+        mpmath.mp.dps = self.dps  # Set precision to 100,000 digits
+        self.pi = str(mpmath.pi)  # Get Pi as a string
         self.index = 2  # Start after the "3."
 
     def get_next_100_digits(self):
         # Get the next 100 digits of Pi
         start = self.index
-        end = start + 100
+        end = start + 100  # Extract exactly 100 digits of Pi
         block = self.pi[start:end]
         self.index += 100
 
         # If precision is too low, increase it for next digits
         if self.index > len(self.pi) - 100:
             self.dps += 100000  # Increase precision in steps of 100,000
-            mpmath.mp.dps = self.dps
+            mpmath.mp.dps = self.dps  # Set the new precision
             self.pi = str(mpmath.pi)  # Recalculate Pi with new precision
 
         # On the first call, include "3." and the next 100 digits
@@ -31,7 +31,7 @@ class PiGood:
 if __name__ == "__main__":
     pi_good = PiGood()
     print("PiGood: Generating Pi digits endlessly... (Ctrl+C to stop)")
-    
+
     while True:
         input("Press Enter to generate next 100 digits of Pi...")
         next_100 = pi_good.get_next_100_digits()
