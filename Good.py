9fb39117442b292de0bd751cef144d573a4d9378
Mohamed Zeidan <mohamedzeidan2012@gmail.com>
2025-08-07 19:50:11 +0200

    Fix Wrong Actual Pi

--- Good.py
+++ Good.py
@@ -2,19 +2,23 @@ import mpmath
 
 # Set the number of digits you want to calculate
 def generate_pi(digits: int):
-    mpmath.mp.dps = digits  # set precision (decimal places)
-    pi_value = str(mpmath.pi)  # get the value of Pi to the specified precision
+    mpmath.mp.dps = digits + 1  # Set precision (1 extra digit to ensure rounding is handled correctly)
+    pi_value = str(mpmath.mp.pi)[2:]  # Get the value of Pi to the specified precision, removing the '3.'
     return pi_value
 
 def main():
     print("PiGood: Generating Pi digits endlessly... (Ctrl+C to stop)")
-    
-    digits_per_request = 100  # Number of digits to generate per request
 
+    digits_per_request = 100  # Number of digits to generate per request
+    generated_digits = ""  # Initialize to store generated digits
+    
     while True:
         input(f"Press Enter to generate next {digits_per_request} digits of Pi...")
-        pi_digits = generate_pi(mpmath.mp.dps + digits_per_request)
-        print(pi_digits[-digits_per_request:])  # Print only the newly generated digits
+        # Append the next batch of digits to the generated digits
+        generated_digits += generate_pi(digits_per_request)
+        
+        # Print only the newly generated digits
+        print(generated_digits[-digits_per_request:])  # This will print only the most recent 100 digits
 
 if __name__ == "__main__":
     main()
